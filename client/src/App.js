091ce900987ee69749797fa7e518f6f2d9b55dae
kpacheco84 <katherinepacheco001@gmail.com>
2019-07-09 12:43:36 -0400

    ADDED SIGNIN
    Sign in not rendering

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,15 @@ import axios from 'axios';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UpdateCourse from './components/UpdateCourse';
-
+import UserSignIn from './components/UserSignIn';
 
 class App extends Component {
 	render() {
     return ( < Router > < Switch > 
 	  < Route path = '/' exact component = {Courses}/>
 	  < Route path = '/courses/:id'exact component = {CourseDetail}/> 
-	  <  Route path = '/courses/:id/update'exact component = {UpdateCourse}/>  
+	  <  Route path = '/courses/:id/update'exact component = {UpdateCourse}/>
+	  < Route path = '/signin'render = {(props) => < UserSignIn props = {props}/>} / >   
 	  </Switch> 
       </Router>);
 		}
--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+//import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
 import axios from 'axios';
 import Header from './Header'
 import { Link } from 'react-router-dom';
